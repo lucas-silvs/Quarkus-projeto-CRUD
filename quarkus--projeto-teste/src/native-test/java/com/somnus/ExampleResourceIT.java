179a21370b8e225616af1e52923306f09812c30c
Lucas Santos <lucas.parkl@outlook.com>
2022-06-13 22:49:09 -0300

    WIP: teste criação endpoint RESTful utilizando Quarkus: - mapeamento de campos do Request Body para um objeto - criação do endpoint /usuario com uma requisição post para o endpoint usuario/cadastrar-usuario e verificado se o mapeamento está correto - criação da classe request que será utilizada para mapear o Request body - criação do response para verificar o retorno

---
+++ quarkus--projeto-teste/src/native-test/java/com/somnus/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.somnus;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
