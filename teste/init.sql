7d671715a95b5c4f76adf5fa87f643d3f53bca06
Lucas Santos <lucas.parkl@outlook.com>
2022-06-15 22:53:05 -0300

    WIP: teste criação endpoint RESTful utilizando Quarkus: - adicionado init.sql para criar usuario e database ao subir o container;

---
+++ teste/init.sql
@@ -0,0 +1,7 @@
+CREATE USER 'wb-quarkus-usuarios'@'%' IDENTIFIED BY 'quarkusdb';
+GRANT ALL PRIVILEGES ON * . * TO 'wb-quarkus-usuarios'@'%';
+CREATE DATABASE db-quarkus;
+GRANT ALL PRIVILEGES ON db_quarkus.* TO 'wb-quarkus-usuarios'@'%';
+FLUSH PRIVILEGES;
+
+
