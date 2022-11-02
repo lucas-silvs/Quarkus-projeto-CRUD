8d94af6760e7add6e55c291d97e0dc2c7ad1d710
Lucas Santos <lucas.parkl@outlook.com>
2022-11-02 15:14:41 -0300

    criando validação de credencial utilizando teclado virtual: - validação passagem da matriz e array para o component

--- quarkus--projeto-teste/src/main/java/com/crudquarkus/components/TecladoVirtualComponent.java
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/components/TecladoVirtualComponent.java
@@ -1,7 +1,16 @@
 package com.crudquarkus.components;
 
+import java.util.Arrays;
+
 public class TecladoVirtualComponent {
     public static boolean isSenhaCorreta(String senha, String[][] tecladoVirtual, int[] teclasPresionadas) {
+        System.out.println("matriz do teclado virtual: "+ Arrays.spliterator(tecladoVirtual));
+        System.out.println("teclas pressionadas: "+ Arrays.toString(teclasPresionadas));
+        System.out.println("senha: " + senha);
         return true;
+    }
+
+     TecladoVirtualComponent() {
+        throw new IllegalStateException("componente não deve ser instanciado: " + this.getClass().getName());
     }
 }
