93d058ab1a6f486e748835823f2a3a46e3770858
Lucas Santos <lucas.parkl@outlook.com>
2022-12-13 23:22:45 -0300

    WIP: implementação de log basico, será melhorado futuramente

---
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/config/LoggingFilter.java
@@ -0,0 +1,41 @@
+package com.crudquarkus.config;
+
+import io.vertx.core.http.HttpServerRequest;
+import io.vertx.core.http.HttpServerResponse;
+import org.jboss.logging.Logger;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+
+@Provider
+public class LoggingFilter implements ContainerResponseFilter {
+
+    private static final Logger LOG = Logger.getLogger(LoggingFilter.class);
+
+    @Context
+    UriInfo info;
+
+    @Context
+    HttpServerRequest request;
+
+    @Context
+    HttpServerResponse response;
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        final String method = requestContext.getMethod();
+        final String path = info.getRequestUri().toString();
+        final String address = request.remoteAddress().toString();
+
+        LOG.infof("Request: %s - Path: %s - body: %s", method, path);
+
+
+
+    }
+}
