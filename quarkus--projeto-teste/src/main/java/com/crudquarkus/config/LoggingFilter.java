0182489d6a5806ebd57e69c5fc4b4b11b1c6d5d1
Lucas Santos <lucas.parkl@outlook.com>
2022-12-25 19:40:47 -0300

    WIP: feat: implementando logging: - implementando Logger manual para Rest Easy Reactive - adicionando dependencia do Log4j no build.gradle - adicionando niveis de logs no application.yaml

--- quarkus--projeto-teste/src/main/java/com/crudquarkus/config/LoggingFilter.java
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/config/LoggingFilter.java
@@ -2,7 +2,8 @@ package com.crudquarkus.config;
 
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
-import org.jboss.logging.Logger;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.JBossLogFactory;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
@@ -11,12 +12,16 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 
 @Provider
 public class LoggingFilter implements ContainerResponseFilter {
 
-    private static final Logger LOG = Logger.getLogger(LoggingFilter.class);
+    private static final Log LOG = JBossLogFactory.getFactory().getInstance(LoggingFilter.class);
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
 
     @Context
     UriInfo info;
@@ -30,10 +35,13 @@ public class LoggingFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
         final String method = requestContext.getMethod();
+        // TODO implementar captura do Request Body para implementação de Log manual
+        final String body = requestContext.getEntityStream().toString();
         final String path = info.getRequestUri().toString();
         final String address = request.remoteAddress().toString();
+        final String timeStamp = formatter.format(LocalDateTime.now());
 
-        LOG.infof("Request: %s - Path: %s", method, path);
+        LOG.info(String.format("%s -- Request: %s - Path: %s -- Body: %s", timeStamp, method, path, body));
 
 
 
