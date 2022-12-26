08fe34e371c1701b4e7c2f68b1b0a5c80d3fe269
Lucas Santos <lucas.parkl@outlook.com>
2022-12-26 19:00:02 -0300

    WIP: feat: implementando logging: - capturando Request body e e tratando para retornar um Json no log info na classe LoggingFilter - implementado validação se possui body, caso não possui será buscado por um query params na classe LoggingFilter - adicionado dependencia do Apache Commons para tratar o body do Entity Stream

--- quarkus--projeto-teste/src/main/java/com/crudquarkus/config/LoggingFilter.java
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/config/LoggingFilter.java
@@ -2,22 +2,26 @@ package com.crudquarkus.config;
 
 import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.impl.JBossLogFactory;
 
 import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 
 @Provider
-public class LoggingFilter implements ContainerResponseFilter {
+public class LoggingFilter implements ContainerRequestFilter, ContainerResponseFilter{
 
     private static final Log LOG = JBossLogFactory.getFactory().getInstance(LoggingFilter.class);
 
@@ -32,18 +36,38 @@ public class LoggingFilter implements ContainerResponseFilter {
     @Context
     HttpServerResponse response;
 
-    @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+
+    public void filter(ContainerRequestContext requestContext) throws IOException {
         final String method = requestContext.getMethod();
-        // TODO implementar captura do Request Body para implementação de Log manual
-        final String body = requestContext.getEntityStream().toString();
         final String path = info.getRequestUri().toString();
-        final String address = request.remoteAddress().toString();
         final String timeStamp = formatter.format(LocalDateTime.now());
 
-        LOG.info(String.format("%s -- Request: %s - Path: %s -- Body: %s", timeStamp, method, path, body));
+        String bodyOrParams = "";
+        if(isJson(requestContext)) {
+            InputStream inputStream = requestContext.getEntityStream();
+            //TODO melhorar implementação de captura do body
+            bodyOrParams = IOUtils.toString(inputStream, String.valueOf(StandardCharsets.UTF_8));
+            requestContext.setEntityStream(IOUtils.toInputStream(bodyOrParams, StandardCharsets.UTF_8));
+            LOG.info(String.format("%s -- Request: %s - Path: %s -- Request Body: %s", timeStamp, method, path, bodyOrParams ));
+        }
+        else{
+            bodyOrParams = String.valueOf(request.query());
+            LOG.info(String.format("%s -- Request: %s - Path: %s -- Request Param: %s", timeStamp, method, path, bodyOrParams ));
+        }
 
+    }
 
+    private boolean isJson(ContainerRequestContext requestContext) {
+        try {
+            return requestContext.getMediaType().toString().contains("application/json");
+        }catch (Exception e){
+            LOG.info(e.getMessage());
+        }
+        return false;
+    }
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
 
     }
 }
