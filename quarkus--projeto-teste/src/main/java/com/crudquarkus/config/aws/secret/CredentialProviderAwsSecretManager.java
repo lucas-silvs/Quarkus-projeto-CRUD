b4307fb3e9044f6152be2eef5ed5a814e383f0ee
Lucas Santos <lucas.parkl@outlook.com>
2022-12-27 15:19:06 -0300

    feat: utilizando secret: - atualizando a implementação de secret manager para utilizar a extensão do Quarkus

--- quarkus--projeto-teste/src/main/java/com/crudquarkus/config/aws/secret/CredentialProviderAwsSecretManager.java
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/config/aws/secret/CredentialProviderAwsSecretManager.java
@@ -5,14 +5,11 @@ import com.google.gson.Gson;
 import io.quarkus.arc.Unremovable;
 import io.quarkus.arc.profile.IfBuildProfile;
 import io.quarkus.credentials.CredentialsProvider;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
 import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,30 +24,24 @@ a busca das credencias
 @IfBuildProfile("secret")
 public class CredentialProviderAwsSecretManager implements CredentialsProvider {
 
-
-    SecretsManagerClient secretsManagerClient;
-
-    @Inject
-    SecretProperties secretProperties;
+    private  final SecretsManagerClient secretsManagerClient;
 
 
-    /*
-    implementação manual para buscar credenciais no secret manager
-     */
+    private final SecretProperties secretProperties;
+
+    public CredentialProviderAwsSecretManager(SecretsManagerClient secretsManagerClient, SecretProperties secretProperties) {
+        this.secretsManagerClient = secretsManagerClient;
+        this.secretProperties = secretProperties;
+    }
+
     @Override
     public Map<String, String> getCredentials(String credentialsProviderName) {
-        this.secretsManagerClient = SecretsManagerClient.builder()
-                //.endpointOverride(new URI(secretProperties.url())) somente para teste local com LocalStack
-                .region(Region.of(secretProperties.region()))
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-
         GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder()
                 .secretId(secretProperties.secretName())
                 .build();
 
         //realiza a busca das credenciais na aws
-        GetSecretValueResponse secretResponse = this.secretsManagerClient.getSecretValue(secretValueRequest);
+        GetSecretValueResponse secretResponse = secretsManagerClient.getSecretValue(secretValueRequest);
 
         //converter o json retornado para um para um Map
         Map<String, String> jsonSecretValue = new Gson().fromJson(secretResponse.secretString(), Map.class);
--- quarkus--projeto-teste/src/main/java/com/crudquarkus/config/aws/secret/SecretProperties.java
+++ quarkus--projeto-teste/src/main/java/com/crudquarkus/config/aws/secret/SecretProperties.java
@@ -5,11 +5,5 @@ import io.smallrye.config.ConfigMapping;
 
 @ConfigMapping(prefix = "aws")
 public interface SecretProperties {
-
     String secretName();
-//    String url(); apenas caso utilize teste local com LocalStack
-    String region();
-
-
-
 }
